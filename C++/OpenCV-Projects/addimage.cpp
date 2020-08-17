43c0e4eb5fe892d48cc0ce492188467a51141a9b
KJ <68377178+kjamal-hub@users.noreply.github.com>
2020-08-17 18:12:54 +0530

    Added addimage.cpp

---
+++ C++/OpenCV-Projects/addimage.cpp
@@ -0,0 +1,59 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <vector>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    double scale = 3.0;
+
+    CascadeClassifier faceCascade;
+    faceCascade.load("C:\\opencv\\build\\install\\etc\\haarcascades\\haarcascade_frontalface_alt.xml");
+
+    VideoCapture cap(0);
+
+    if(!cap.isOpened())
+        return -1;
+
+    for(;;)
+   {
+       Mat Frame;
+       cap >> Frame;
+
+       Mat grayscale;
+       cvtColor(Frame, grayscale, COLOR_BGR2GRAY);
+       resize(grayscale, grayscale, Size(grayscale.size().width / scale, grayscale.size().height / scale));
+
+       vector<Rect> faces;
+
+       faceCascade.detectMultiScale(grayscale, faces, 1.1, 3, 0, Size(30, 30));
+
+       for(Rect area: faces)
+       {
+           Scalar drawColor = Scalar(255, 0, 0);
+           rectangle(Frame, Point(cvRound(area.x * scale), cvRound(area.y * scale)),
+                     Point(cvRound(area.x + area.width - 1) * scale, cvRound(area.y + area.height - 1) * scale), drawColor);
+       }
+
+       namedWindow("Face-Detect", WINDOW_AUTOSIZE);
+
+       imshow("Face-Detect", Frame);
+
+       if(waitKey(1) >= 0)
+          break;
+   }
+
+    Mat save_img;
+    cap >> save_img;
+
+    if(save_img.empty())
+    {
+        cerr << "Something is wrong with the webcam, could not get frame." << endl;
+    }
+
+    imwrite("test.jpg", save_img);
+
+    return 0;
+}
